b6d39a122305faba034c62307cb5cf3d5e05a4ad
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-30 15:05:33 -0700

    changed init.py org

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,8 +4,8 @@ import json
 pets =json.load(open('pets.json'))
 print(pets)
 
-from .pet import bp as pet_bp
-from .fact import bp as fact_bp 
+
+
 # the factory function
 def create_app():
     app = Flask(__name__)
@@ -14,8 +14,10 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
-    
+    from .pet import bp as pet_bp
     app.register_blueprint(pet_bp)
+
+    from .fact import bp as fact_bp 
     app.register_blueprint(fact_bp)
 
     return app
