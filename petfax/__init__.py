17eb15258dae93d5e24db0435074609d36bae8a4
Kolby Kiernan <kolbykiernan@gmail.com>
2024-03-30 15:03:45 -0700

    finish assignment

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
+import json
 
+pets =json.load(open('pets.json'))
+print(pets)
+
+from .pet import bp as pet_bp
+from .fact import bp as fact_bp 
 # the factory function
 def create_app():
     app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
     
-    from . import pet
-    app.register_blueprint(pet.bp)
+    app.register_blueprint(pet_bp)
+    app.register_blueprint(fact_bp)
 
     return app
+
+
     
--- petfax/pet.py
+++ petfax/pet.py
@@ -9,3 +9,10 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index():
     return render_template('index.html', pets=pets)
+
+@bp.route('/<pet_id>')
+def show(pet_id):
+    pet_id = int(pet_id)
+    if pet_id > 0 and pet_id <= len(pets):
+        pet_index = pet_id - 1
+    return render_template('show.html', pet=pets[pet_index])
